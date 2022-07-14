d411a32c6516854cde4a87562c96b69aab5b1e5d
Gazi Fayaz Ahmed <gazifayaz.16694@gmail.com>
2022-07-14 16:56:48 +0600

    Add logo to header

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import {
   HomeIcon,
   SearchIcon,
@@ -10,29 +11,31 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 flex items-center bg-header px-4 py-1 h-14">
-      {/* // "mistri" logo left side */}
-      <div className="flex items-center pr-5">
-        <h1 className=" link font-bold text-2xl cursor-pointer">
-          mistri
-        </h1>
-      </div>
+    <header>
+      <div className="fixed top-0 left-0 right-0 flex items-center bg-header px-4 py-1 h-14">
+        {/* "mistri" logo left side */}
+        <Link href={"/"}>
+          <div className="flex items-center pr-7 pb-2">
+            <Image src="/mistri_logo_svg.svg" height={55} width={55} className="cursor-pointer" />
+          </div>
+        </Link>
 
-      {/* Search bar */}
-      <div className="flex items-center rounded-full bg-slate-200 h-8 w-auto">
-        <SearchIcon className="h-14 pl-4 pt-4 pb-4 cursor-pointer rounded-md hover:stroke-emerald-500" />
-        <input
-          type="text"
-          placeholder="search services"
-          className=" rounded-md focus:outline-none text-left bg-slate-200 text-black mr-5 pl-3 w-60"
-        />
-      </div>
+        {/* Search bar */}
+        <div className="flex items-center rounded-full bg-slate-200 h-8 w-auto">
+          <SearchIcon className="h-14 pl-4 pt-4 pb-4 cursor-pointer rounded-md hover:stroke-emerald-500" />
+          <input
+            type="text"
+            placeholder="search services"
+            className=" rounded-md focus:outline-none text-left bg-slate-200 text-black mr-5 pl-3 w-60"
+          />
+        </div>
 
-      {/* // header-right-side */}
-      <div className="flex items-center space-x-3 px-4 fixed right-0">
-        <Navbar />
+        {/* header-right-side */}
+        <div className="flex items-center space-x-3 fixed right-0">
+          <Navbar />
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
