a41fce54fff21f62462515bd7db6182b9ca77b5d
Sakibfootball <sakibhaider20@gmail.com>
2022-07-19 00:40:21 +0600

    sanity installation and initialization complete sanity dir created

---
+++ sanity/schemas/services.js
@@ -0,0 +1,19 @@
+export default {
+  name: "services",
+  title: "Services",
+  type: "document",
+  fields: [
+    {
+      name: "services",
+      title: "Services",
+      type: "array",
+      of: [
+        {
+          name: "services",
+          title: "Services",
+          type: "string",
+        }
+      ]
+    },
+  ],
+};
