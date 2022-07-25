a4ab1e37480c010d39eec29e7d9b9e6c6be4a8cf
Gazi Fayaz Ahmed <gazifayaz.16694@gmail.com>
2022-07-25 12:05:32 +0600

    Create schema for mistri application form

---
+++ sanity/schemas/mistri-application.js
@@ -0,0 +1,43 @@
+export default {
+    name: "application",
+    title: "Application",
+    type: "document",
+    fields: [
+      {
+        name: "first name",
+        title: "First Name",
+        type: "string",
+      },
+      {
+        name: "last name",
+        title: "Last Name",
+        type: "string",
+      },
+      {
+        name: "email",
+        title: "Email",
+        type: "string",
+      },
+      {
+        name: "phone number",
+        title: "Phone Number",
+        type: "",
+      },
+      {
+        name: "email",
+        title: "Email",
+        type: "string",
+      },
+      {
+        name: "email",
+        title: "Email",
+        type: "string",
+      },
+      {
+        name: "email",
+        title: "Email",
+        type: "string",
+      },
+    ],
+  };
+  
