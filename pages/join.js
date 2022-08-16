ac2364608626a1165e4b394ff8d86f768256b177
Gazi Fayaz Ahmed <gazifayaz.16694@gmail.com>
2022-08-16 20:01:16 +0600

    add react state hooks and finalised input fields with not working file inputs

--- pages/join.js
+++ pages/join.js
@@ -1,9 +1,51 @@
 import Image from "next/image";
 import Link from "next/link";
+import { ArrowDownIcon } from "@heroicons/react/solid";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import React, { useState } from "react";
+import { sanityClient } from "../lib/Sanity";
+import { ThemeProvider } from "next-themes";
 
 import mistrilogo from "../public/mistri_logo_svg.svg";
+import Multiselect from "multiselect-react-dropdown";
 
 const join = () => {
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setlastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone_number, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState();
+  const [expertises, setExpertises] = useState([
+    {
+      key: "Carpentry",
+    },
+    {
+      key: "Paintings",
+    },
+    {
+      key: "Tile Work",
+    },
+    {
+      key: "Waterline",
+    },
+  ])
+  const [experience, setExperience] = useState(null);
+  const [certificates, setCertificates] = useState([
+    {
+      id: 0,
+      value:"dummy"
+    },
+    {
+      id:1,
+      value: "stupid",
+    }
+  ]);
+  const test = (e) => {
+    console.log(e);
+  };
+
+  // const handleOnSubmit = ()
   return (
     <div>
       <div className="md:inline-flex sm:flex-col bg-homebg w-screen h-screen">
@@ -40,70 +82,91 @@ const join = () => {
             <h2 className="form-heading text-3xl text-center font-bold">
               Apply for Interview
             </h2>
-            <form className="form pt-8 space-y-4 ">
+            <form className="form pt-8 space-y-4 " >
               <div className="name flex space-x-2 ">
                 <input
                   placeholder="First Name"
                   type="text"
                   required
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                  }}
                   className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:border-green-700 focus:z-10 sm:text-sm"
                 />
                 <input
                   placeholder="Last Name"
                   type="text"
                   required
+                  onChange={(e) => {
+                    setlastName(e.target.value);
+                  }}
                   className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:border-green-700 focus:z-10 sm:text-sm"
                 />
               </div>
               <input
                 placeholder="Email"
+                // onChange={handleOnChangeEmail}
                 type="Email"
                 required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
                 className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:border-green-700 focus:z-10 sm:text-sm"
               />
               <input
+                id
                 placeholder="Phone Number"
                 type="text"
                 required
+                onChange={(e) => {
+                  setPhoneNumber(e.target.value);
+                }}
                 className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:border-green-700 focus:z-10 sm:text-sm"
               />
               <input
                 placeholder="Address"
                 type="Address"
                 required
+                onChange={(e) => {
+                  setAddress(e.target.value);
+                }}
                 className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:border-green-700 focus:z-10 sm:text-sm"
               />
               <input
                 placeholder="Date of Birth"
                 type="Date"
                 required
+                onChange={(e) => {
+                  setDateOfBirth(e.target.value);
+                }}
                 className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:border-green-700 focus:z-10 sm:text-sm"
               />
-              <input
-                list="Expertise_selector"
-                name="browser"
-                placeholder="Select Expertise Category"
-                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:border-green-700 focus:z-10 sm:text-sm"
-              />
-              <datalist id="Expertise_selector">
-                <option value="Carpentry" className="text-extrabold" />
-                <option value="Painting" />
-                <option value="Electrician" />
-                <option value="Waterline" />
-                <option value="Sweeping" />
-                <option value="Electrician" />
-                <option value="Waterline" />
-                <option value="Sweeping" />
-                <option value="Electrician" />
-                <option value="Waterline" />
-                <option value="Sweeping" />
-                <option value="Electrician" />
-                <option value="Waterline" />
-                <option value="Sweeping" />
-                <option value="Electrician" />
-                <option value="Waterline" />
-                <option value="Sweeping" />
-              </datalist>
+              {/* <div className="expertise-select flex "> */}
+                <Multiselect
+                  className="bg-white rounded-lg"
+                  id="css_custom"
+                  displayValue="key"
+                  onKeyPressFn={function noRefCheck() {}}
+                  onSelect={(event)=> {console.log(event)}}
+                  onRemove={(event)=> {console.log(event)}}
+                  // onSearch={(event)=> {console.log(event)}}
+                  hidePlaceholder="true"
+                  avoidHighlightFirstOption="false"
+                  closeIcon="cancel"
+                  placeholder="Choose Expertise(s)"
+                  options={expertises}
+                  style={{
+                    chips : { // To change css for dropdown options
+                      background: "#00D3AD",
+                      color: "black"
+                      },
+                  }
+                    
+                  }
+                />
+                {/* <ChevronDownIcon className="relative right-8 top-2 w-5 h-5 pointer-events-none stroke-gray-300" /> */}
+              {/* </div> */}
+
               <input
                 placeholder="Experience (years)"
                 type="number"
@@ -120,10 +183,34 @@ const join = () => {
                 <input
                   id="certificate"
                   type="file"
+                  onChange= { (event) => {
+                    // setCertificates([...certificates, {
+                    //   id: certificates.length,
+                    //   value: event.target.value
+                    // }])
+                    //   certificates.map(certificate => {
+                    //     console.log(certificate.value)
+                    // })
+                    console.log(event.target.files);
+                    setCertificates([... certificates, 
+                      {
+                        id: certificates.length,
+                        value: "event.target.files",
+                      }
+                    ])
+                  } }
+                  
                   accept=".pdf, image/png, image/jpg, image/jpeg"
                   className="bg-gray-100 text-gray-500 rounded-r-md"
                   multiple
                 />
+                <ul>
+                  {
+                    certificates.map(certificate => (
+                      <li key={certificate.id}>{certificate.value}</li>
+                    ))
+                  }
+                </ul>
               </div>
               <div className="rounded-full">
                 <label
@@ -144,7 +231,7 @@ const join = () => {
 
               <button
                 type="submit"
-                onClick={""}
+                onClick={test}
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-lg font-bold rounded-md text-green-900 hover:text-black bg-header hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-header "
               >
                 Submit
