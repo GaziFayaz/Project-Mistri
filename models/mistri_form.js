c3048ef48a8534c6d0ca192cdf6ed416bae77474
Gazi Fayaz Ahmed <gazifayaz.16694@gmail.com>
2022-08-16 19:47:45 +0600

    mongo schema for application form

---
+++ models/mistri_form.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const FormSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    maxlength: 25,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    maxlength: 25,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    maxlength: 11,
+  },
+  address: {
+    type: String,
+    required: true,
+    maxlength: 35,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  expertises: {
+    type: [String],
+    required: true,
+  },
+  experience: {
+    type: Number,
+    required: true,
+  },
+  certificates: {
+    type: [
+      {
+        certificate: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+});
