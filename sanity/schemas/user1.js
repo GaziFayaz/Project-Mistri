3614987f56418fa8a49aabdbeeb4e9fe0c37f705
Gazi Fayaz Ahmed <gazifayaz.16694@gmail.com>
2022-08-23 05:35:37 +0600

    add new attributes to user schema

--- sanity/schemas/user1.js
+++ sanity/schemas/user1.js
@@ -4,8 +4,38 @@ export default {
   type: "document",
   fields: [
     {
+      name: "first_name",
+      title: "First Name",
+      type: "string",
+    },
+    {
+      name: "last_name",
+      title: "Last Name",
+      type: "string",
+    },
+    {
       name: "email",
       title: "Email",
+      type: "string",
+    },
+    {
+      name: "phone_number",
+      title: "Phone Number",
+      type: "string",
+    },
+    {
+      name: "address",
+      title: "Address",
+      type: "string",
+    },
+    {
+      name: "dateOfBirth",
+      title: "Date of Birth",
+      type: "date",
+    },
+    {
+      name: "photo",
+      title: "Photo",
       type: "string",
     },
   ],
